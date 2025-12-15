1b1ee35c380e9dcd304e20b60a2502b3699fc0b6
Ivan BOLTENKOV <ivanwrest@gmail.com>
2025-12-15 23:58:15 +0200

    feat: complete task – final version implemented

--- js/task-4.js
+++ js/task-4.js
@@ -1,20 +1,17 @@
 const form = document.querySelector('.login-form');
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault(); 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
   const formElements = event.target;
-  const email = formElements.email.value;
-  const password = formElements.password.value;
+  const email = formElements.email.value.trim();
+  const password = formElements.password.value.trim();
 
-  if (email === "" || password === "") {
-    alert("All form fields must be filled in");
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
     return;
   }
 
-  const formData = {
-    email: email.trim(),
-    password: password.trim()
-  };
+  const formData = { email, password };
 
   console.log(formData);
   form.reset();
